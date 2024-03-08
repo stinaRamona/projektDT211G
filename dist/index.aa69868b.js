96b7b847ff30e84b64f53eda559a0836f8618ad2
Stina Persson <stpe1901@student.miun.se>
2024-03-08 16:17:03 +0100

    valt att testa ny ide då jag inte riktigt fick till apierna på förra. Lagt in ny api och påbörjat lösning

--- dist/index.aa69868b.js
+++ dist/index.aa69868b.js
@@ -1,40 +1,27 @@
-/*Jag vill att man ska kunna söka på ett konstverk och att 
-det visas vart det befinner sig. Då behöver jag dels koppla query-till sökvärdet 
-i sökbaren, samt koppla url.dataprovider till nominatium för att få upp det på en karta. */ /*Ska visas när man sökt på konstverket: 
-*dcTitleLangAware 
-* dcDescription 
-*/ let searchBtnEl = document.getElementById("searchBtn");
+"use strict";
+let searchBtnEl = document.getElementById("searchBtn");
 searchBtnEl.addEventListener("click", getValue);
 //Tar värdet från searchbar och sätter in det i URL för API:n 
 function getValue() {
     let searchbarValue = document.getElementById("searchBar").value;
-    let artURL = `https://api.europeana.eu/record/v2/search.json?wskey=idgelielo&query=${searchbarValue}`;
-    getArtInfo(artURL);
+    let bookURL = `https://api.bigbookapi.com/search-books?api-key=e6f1f17954b54f6bbc6cb857bc9bfb82&query=${searchbarValue}`;
+    getBookInfo(bookURL);
 }
 //Hämtar API:n med sökvärdet i 
-async function getArtInfo(artURL) {
+async function getBookInfo(bookURL) {
     try {
-        let response = await fetch(artURL);
-        let artInfo = await response.json();
-        let artInfoPlace = artInfo.items[0].country[0]; // får ut museet där det finns (för första alternativet) - kan sättas in i nominatum! 
-        console.table(artInfoPlace);
-        getMapInfo(artInfoPlace); // det fungerar att få in landet, men inte själva museet. Kanske behöver testa en annan karttjänst. 
+        let response = await fetch(bookURL);
+        let bookInfo = await response.json();
+        console.table(bookInfo.books.title);
     } catch  {
         console.log("N\xe5got gick fel...");
     }
 }
-async function getMapInfo(artInfoPlace) {
-    try {
-        let mapURL = `https://www.google.com/maps/embed/v1/search?q=${artInfoPlace}&key=`;
-        let response = await fetch(mapURL);
-        let mapInfo = await response.json();
-        document.getElementsByTagName("iframe")[0].src = mapInfo;
-    //console.table(mapInfo.features[0].geometry.coordinates); 
-    } catch  {
-        console.log("kunde inte l\xe4sa in information...");
-    }
-} //UcDZGnfQh4LGU3kPhDQO ID
- //gtwDsWMPhuTrBH7k2ps5IGlC8b_ZxPOnGhwMw-ZVUFc KEY
- // G key AIzaSyA5jgtp-GaWnXrNI1dveaqERx0OEMqsUzM 
-
+ /*
+function displayBookInfo(bookInfo) {
+    bookInfo.forEach(book => {
+        
+    });
+}
+*/ 
 //# sourceMappingURL=index.aa69868b.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,4 @@
-/*Jag vill att man ska kunna söka på ett konstverk och att 
-det visas vart det befinner sig. Då behöver jag dels koppla query-till sökvärdet 
-i sökbaren, samt koppla url.dataprovider till nominatium för att få upp det på en karta. */
-
-/*Ska visas när man sökt på konstverket: 
-*dcTitleLangAware 
-* dcDescription 
-*/ 
+ "use strict"; 
 
 let searchBtnEl = document.getElementById("searchBtn"); 
 
@@ -15,47 +8,29 @@ searchBtnEl.addEventListener('click', getValue);
 function getValue() {
     let searchbarValue = document.getElementById("searchBar").value; 
 
-    let artURL = `https://api.europeana.eu/record/v2/search.json?wskey=idgelielo&query=${searchbarValue}`;
+    let bookURL = `https://api.bigbookapi.com/search-books?api-key=e6f1f17954b54f6bbc6cb857bc9bfb82&query=${searchbarValue}`;
 
-    getArtInfo(artURL)
+    getBookInfo(bookURL)
 }
 
 //Hämtar API:n med sökvärdet i 
-async function getArtInfo(artURL) {
+async function getBookInfo(bookURL) {
     try {
-        let response = await fetch(artURL); 
+        let response = await fetch(bookURL); 
 
-        let artInfo = await response.json(); 
+        let bookInfo = await response.json(); 
 
-        let artInfoPlace = artInfo.items[0].country[0]; // får ut museet där det finns (för första alternativet) - kan sättas in i nominatum! 
-
-        console.table(artInfoPlace); 
-
-        getMapInfo(artInfoPlace); // det fungerar att få in landet, men inte själva museet. Kanske behöver testa en annan karttjänst. 
+        console.table(bookInfo.books.title); 
+ 
     } catch {
         console.log("Något gick fel...")
     }
-}; 
+};  
 
-async function getMapInfo(artInfoPlace){
-    try {
-        let mapURL = `https://www.google.com/maps/embed/v1/search?q=${artInfoPlace}&key=`; 
-
-        let response = await fetch(mapURL); 
-
-        let mapInfo = await response.json(); 
-
-        document.getElementsByTagName("iframe")[0].src = mapInfo; 
-
-        //console.table(mapInfo.features[0].geometry.coordinates); 
-
-    } catch {
-        console.log("kunde inte läsa in information...")
-    }
+/*
+function displayBookInfo(bookInfo) {
+    bookInfo.forEach(book => {
+        
+    });
 }
-
-//UcDZGnfQh4LGU3kPhDQO ID
-
-//gtwDsWMPhuTrBH7k2ps5IGlC8b_ZxPOnGhwMw-ZVUFc KEY
-
-// G key AIzaSyA5jgtp-GaWnXrNI1dveaqERx0OEMqsUzM 
+*/
