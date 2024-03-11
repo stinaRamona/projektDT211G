16303ba49e476f1e7b66f9bae18d4cddf4af213a
Stina Persson <stpe1901@student.miun.se>
2024-03-11 10:49:30 +0100

    Fått fram ID för böckerna och skrivit funktion som tar emot ID:t och hämtar information om böckerna. Kvar är att skriva ut informationen som jag får från funktion nummer 2.

--- dist/index.aa69868b.js
+++ dist/index.aa69868b.js
@@ -5,23 +5,41 @@ searchBtnEl.addEventListener("click", getValue);
 function getValue() {
     let searchbarValue = document.getElementById("searchBar").value;
     let bookURL = `https://api.bigbookapi.com/search-books?api-key=e6f1f17954b54f6bbc6cb857bc9bfb82&query=${searchbarValue}`;
-    getBookInfo(bookURL);
+    getBookID(bookURL);
 }
-//Hämtar API:n med sökvärdet i 
-async function getBookInfo(bookURL) {
+//Hämtar API:n med sökvärdet i och får fram ID på boken för att kunna få mer info
+async function getBookID(bookURL) {
     try {
         let response = await fetch(bookURL);
         let bookInfo = await response.json();
-        console.table(bookInfo.books.title);
+        let bookArray = bookInfo.books;
+        bookArray.forEach((book)=>{
+            let bookId = book[0].id;
+            getBookInfo(bookId);
+        });
     } catch  {
         console.log("N\xe5got gick fel...");
+    }
+}
+async function getBookInfo(bookId) {
+    let InfoURL = `https://api.bigbookapi.com/${bookId}?api-key=e6f1f17954b54f6bbc6cb857bc9bfb82`;
+    try {
+        let response = await fetch(InfoURL);
+        let bookDescription = response.json();
+        console.log(bookDescription);
+    } catch  {
+        console.log("N\xe5got gick snett");
     }
 }
  /*
 function displayBookInfo(bookInfo) {
     bookInfo.forEach(book => {
         
+        document.getElementById("bookList").innerHTML = `
+        <li>${book.title}</li>
+        <li>${book.image}</li>
+        <li>${book.id}</li> 
+        ` 
     });
-}
-*/ 
+}*/ 
 //# sourceMappingURL=index.aa69868b.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -10,27 +10,52 @@ function getValue() {
 
     let bookURL = `https://api.bigbookapi.com/search-books?api-key=e6f1f17954b54f6bbc6cb857bc9bfb82&query=${searchbarValue}`;
 
-    getBookInfo(bookURL)
+    getBookID(bookURL)
 }
 
-//Hämtar API:n med sökvärdet i 
-async function getBookInfo(bookURL) {
+//Hämtar API:n med sökvärdet i och får fram ID på boken för att kunna få mer info
+async function getBookID(bookURL) {
     try {
         let response = await fetch(bookURL); 
 
         let bookInfo = await response.json(); 
 
-        console.table(bookInfo.books.title); 
+        let bookArray = bookInfo.books; 
+
+        bookArray.forEach(book => {
+            let bookId = book[0].id;  
+
+            getBookInfo(bookId);
+        }); 
  
     } catch {
         console.log("Något gick fel...")
     }
 };  
 
+async function getBookInfo(bookId) {
+    let InfoURL = `https://api.bigbookapi.com/${bookId}?api-key=e6f1f17954b54f6bbc6cb857bc9bfb82`; 
+
+    try {
+        let response = await fetch(InfoURL); 
+
+        let bookDescription = response.json(); 
+
+        console.log(bookDescription); 
+    } catch {
+        console.log("Något gick snett")
+    }
+}; 
+
+
 /*
 function displayBookInfo(bookInfo) {
     bookInfo.forEach(book => {
         
+        document.getElementById("bookList").innerHTML = `
+        <li>${book.title}</li>
+        <li>${book.image}</li>
+        <li>${book.id}</li> 
+        ` 
     });
-}
-*/
+}*/
